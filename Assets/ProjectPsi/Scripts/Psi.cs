c1b4756b5da5383bc4edae2ebf515b316dc19202
theishiopian <theishiopian@gmail.com>
2020-10-03 21:42:13 -0700

    Staging stuff

---
+++ Assets/ProjectPsi/Scripts/Psi.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Psi : MonoBehaviour
+{
+    public SteamVR_Action_Boolean psiButton;
+
+    public SteamVR_Input_Sources controllers;
+
+    private bool psiActive = false;
+    private float psiLevel = 10;
+
+    private void Awake()
+    {
+        GlobalVars.playerPsi = this;
+    }
+
+    void Update()
+    {
+        psiActive = psiButton.GetState(controllers);
+    }
+
+    public bool GetPsiActive()
+    {
+        return psiActive;
+    }
+
+    public float GetPsi()
+    {
+        return psiLevel;
+    }
+
+    public void ModifyPsi(float amount)
+    {
+        psiLevel = Mathf.Clamp(psiLevel + amount, 10, 100);
+    }
+}
