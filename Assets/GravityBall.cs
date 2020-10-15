f81a7eb4ec8cee1c84a4679d25babb5b2564004d
theishiopian <theishiopian@gmail.com>
2020-10-14 19:57:35 -0700

    More Gravity Experiments

---
+++ Assets/GravityBall.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Labs.SuperScience;
+using UnityEngine;
+
+public class GravityBall : MonoBehaviour
+{
+    public float radius = 5;
+    public float gravity = 9.81f;
+
+    private PhysicsTracker tracker;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tracker = new PhysicsTracker();
+        this.gameObject.SetActive(false);
+    }
+
+    Collider[] objects;
+    Rigidbody body;
+
+    private void FixedUpdate()
+    {
+        tracker.Update(transform.position, transform.rotation, Time.fixedDeltaTime);
+        objects = Physics.OverlapSphere(transform.position, radius);
+
+        for(int i = 0; i != objects.Length; i++)
+        {
+            body = objects[i].GetComponent<Rigidbody>();
+
+            if(body != null)
+            {
+                body.AddForce((body.position - transform.position).normalized * gravity / Time.fixedDeltaTime, ForceMode.Acceleration);
+            }
+        }
+    }
+}
---
+++ Assets/GravityDriver.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Labs.SuperScience;
+using UnityEngine;
+using Valve.VR;
+
+public class GravityDriver : MonoBehaviour
+{
+    public Transform head;
+
+    public Transform hand;
+
+    public SteamVR_Input_Sources controller;
+    public SteamVR_Action_Boolean trigger;
+
+    public GameObject gravitySphere;
+
+    public float speedFactor = 10f;
+
+    private PhysicsTracker handTracker;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        handTracker = new PhysicsTracker();
+    }
+
+    private RaycastHit hit;
+
+    private void FixedUpdate()
+    {
+        handTracker.Update(hand.position, hand.rotation, Time.fixedDeltaTime);
+        if (trigger.GetStateDown(controller))
+        {
+            if (Physics.Raycast(head.position, head.forward, out hit, 10f))
+            {
+                gravitySphere.transform.position = hit.point;
+                gravitySphere.SetActive(true);
+            }
+        }
+
+        if (trigger.GetState(controller))
+        {
+            gravitySphere.transform.position += handTracker.Velocity * Time.fixedDeltaTime * speedFactor;
+        }
+
+        if (trigger.GetStateUp(controller))
+        {
+            gravitySphere.SetActive(false);
+        }
+    }
+}
