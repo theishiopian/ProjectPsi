3129483601c4a02c58c29ed9c36c9bb20f9f12da
theishiopian <theishiopian@gmail.com>
2020-10-18 19:51:56 -0700

    Added heartbeat vignette when health is below 30

--- Assets/ProjectPsi/Scripts/Extensions.cs
+++ Assets/ProjectPsi/Scripts/Extensions.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public static class Extensions
 {
-    public static float ReMap(this float input, float inputMin, float inputMax, float min, float max)
+    public static float Remap(this float input, float inputMin, float inputMax, float min, float max)
     {
         return min + (input - inputMin) * (max - min) / (inputMax - inputMin);
     }
