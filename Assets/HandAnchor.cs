52693fd6eac6dab4cb42afd242dd48e98d58114b
theishiopian <theishiopian@gmail.com>
2020-11-03 14:04:06 -0800

    Added hand anchors

---
+++ Assets/HandAnchor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandAnchor : MonoBehaviour
+{
+    private Transform hand;
+
+    private void Start()
+    {
+        hand = GlobalVars.Get("right_hand").transform;
+    }
+    private void FixedUpdate()
+    {
+        this.transform.rotation = Quaternion.identity;
+        this.transform.position = hand.position;
+    }
+}
