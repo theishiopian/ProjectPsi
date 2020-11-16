fb18c395104697a21164b283b01948f9224c0fb6
theishiopian <theishiopian@gmail.com>
2020-11-15 18:17:36 -0800

    WHO KEEPS UNPACKING THE PLAYER???????????

---
+++ Assets/FollowAI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowAI : MonoBehaviour
+{
+    private NavMeshAgent agent;
+    private Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        player = GlobalVars.Get("player_rig").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
