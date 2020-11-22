e63afc7646722e082bcece3fc07272674e932e19
theishiopian <theishiopian@gmail.com>
2020-11-21 20:38:11 -0800

    Implemented Teleport Arc

--- Assets/ProjectPsi/Scripts/PlayerMovement.cs
+++ Assets/ProjectPsi/Scripts/PlayerMovement.cs
@@ -4,16 +4,16 @@ using Valve.VR;
 public class PlayerMovement : MonoBehaviour
 {
     public SteamVR_Input_Sources movementHand;
-    public SteamVR_Input_Sources teleportHand;
+    public SteamVR_Input_Sources turnHand;
     public SteamVR_Action_Vector2 movementJoystick;
     public SteamVR_Action_Boolean snapLeftAction;
     public SteamVR_Action_Boolean snapRightAction;
-    public SteamVR_Action_Boolean teleportAction;
+    //public SteamVR_Action_Boolean teleportAction;
     public bool useHand;//if true we use your hand for movement. if false we use the head. default false
     public float speed;
     public float deadzone;//the Deadzone of the joystick. used to prevent unwanted walking.
-    public LineRenderer teleportLine;
-    public Transform teleportReticle;
+    //public LineRenderer teleportLine;
+    //public Transform teleportReticle;
 
     private Transform head;
     private Transform leftHand;
@@ -70,30 +70,30 @@ public class PlayerMovement : MonoBehaviour
             body.rotation = body.rotation * Quaternion.Euler(0, 15, 0);
         }
 
-        if (teleport)
-        {
-            shouldTeleport = true;
+        //if (teleport)
+        //{
+        //    shouldTeleport = true;
 
-            if (Physics.Raycast(rightHand.position, rightHand.forward - rightHand.up, out tpHit, 10f))
-            {
-                teleportReticle.gameObject.SetActive(true);
-                teleportReticle.position = tpHit.point;
-                teleportLine.enabled = true;
-                teleportLine.SetPositions(new Vector3[] {rightHand.position, tpHit.point});
-            }
-        }
-        else if (shouldTeleport)
-        {
-            shouldTeleport = false;
-            body.position = teleportReticle.position + new Vector3(0, 0.1f, 0);
-            teleportReticle.gameObject.SetActive(false);
-            teleportLine.enabled = false;
-        }
-        else
-        {
-            teleportReticle.gameObject.SetActive(false);
-            teleportLine.enabled = false;
-        }
+        //    if (Physics.Raycast(rightHand.position, rightHand.forward - rightHand.up, out tpHit, 10f))
+        //    {
+        //        teleportReticle.gameObject.SetActive(true);
+        //        teleportReticle.position = tpHit.point;
+        //        teleportLine.enabled = true;
+        //        teleportLine.SetPositions(new Vector3[] {rightHand.position, tpHit.point});
+        //    }
+        //}
+        //else if (shouldTeleport)
+        //{
+        //    shouldTeleport = false;
+        //    body.position = teleportReticle.position + new Vector3(0, 0.1f, 0);
+        //    teleportReticle.gameObject.SetActive(false);
+        //    teleportLine.enabled = false;
+        //}
+        //else
+        //{
+        //    teleportReticle.gameObject.SetActive(false);
+        //    teleportLine.enabled = false;
+        //}
     }
 
     public static float GetAngle(Vector2 p_vector2)//put this in a library???
@@ -121,8 +121,8 @@ public class PlayerMovement : MonoBehaviour
         //todo catch any exceptions from turning controller offf
         joystickInput = movementJoystick.GetAxis(movementHand);
 
-        snapLeft = snapLeftAction.GetState(teleportHand);
-        snapRight = snapRightAction.GetState(teleportHand);
-        teleport = teleportAction.GetState(teleportHand);
+        snapLeft = snapLeftAction.GetState(turnHand);
+        snapRight = snapRightAction.GetState(turnHand);
+        //teleport = teleportAction.GetState(teleportHand);
     }
 }
---
+++ Assets/ProjectPsi/Scripts/Teleport.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+using Valve.VR.InteractionSystem;
+
+public class Teleport : MonoBehaviour
+{
+    public SteamVR_Input_Sources controller;
+    public SteamVR_Action_Boolean teleportAction;
+    public Transform teleportHand;
+    public TeleportArc arc;
+    public LayerMask arcMask;
+    public new CapsuleCollider collider;
+    public Rigidbody body;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        arc.Show();
+        arc.traceLayerMask = arcMask;
+    }
+    RaycastHit hit;
+
+    bool shouldTeleport = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        arc.SetArcData(teleportHand.position, teleportHand.forward * 10, true, false);
+        bool didHit = arc.DrawArc(out hit);
+
+        if(didHit && !hit.collider.gameObject.layer.Equals(LayerMask.NameToLayer("NavMesh")))
+        {
+            arc.SetColor(Color.red);
+        }
+        else
+        {
+            arc.SetColor(Color.blue);
+        }
+    }
+
+    //can we fit to the destination?
+    bool CanTeleportTo(Vector3 destination)
+    {
+        return true;
+    }
+
+
+}
