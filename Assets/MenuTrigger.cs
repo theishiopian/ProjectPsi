d35fa05ad67dcdd19f81550b9f29f10a6ada7c39
theishiopian <theishiopian@gmail.com>
2020-11-24 15:58:53 -0800

    Menu system can now detect markers

---
+++ Assets/MenuTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void MenuCallback();
+
+public class MenuTrigger : MonoBehaviour
+{
+    public MenuCallback menuCallback;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Marker"))
+        {
+            Debug.Log("detected");
+            if(menuCallback != null)menuCallback();
+        }
+    }
+}
