95ebd3d8bc3d2bb264111b78569b5369f5c2f9a6
theishiopian <theishiopian@gmail.com>
2020-11-24 16:49:06 -0800

    Whiteboard demo

--- Assets/MenuTrigger.cs
+++ Assets/MenuTrigger.cs
@@ -1,19 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public delegate void MenuCallback();
+using UnityEngine.Events;
 
 public class MenuTrigger : MonoBehaviour
 {
-    public MenuCallback menuCallback;
+    [System.Serializable]
+    public class MenuCallback : UnityEvent { }
+
+    public MenuCallback callback;
 
     private void OnTriggerExit(Collider other)
     {
         if(other.CompareTag("Marker"))
         {
             Debug.Log("detected");
-            if(menuCallback != null)menuCallback();
+            callback.Invoke();
         }
     }
 }
