34bbbd9be8d2bc08e8997ea92d6aaec17f3a8ebd
theishiopian <theishiopian@gmail.com>
2020-12-04 23:40:32 -0800

    connected event callbacks
    -implemented saving of settings as well

--- Assets/OptionsMenu.cs
+++ Assets/OptionsMenu.cs
@@ -4,16 +4,12 @@ using UnityEngine;
 
 public class OptionsMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnEnable()
     {
-        
-    }
+        if (!PlayerPrefs.HasKey("volume")) PlayerPrefs.SetFloat("volume", 1.0f);
+        if (!PlayerPrefs.HasKey("smoothmove")) PlayerPrefs.SetInt("smoothmove", 1);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        AudioListener.volume = PlayerPrefs.GetFloat("volume");
     }
 
     public void EditVolume(string input)
@@ -23,15 +19,36 @@ public class OptionsMenu : MonoBehaviour
             case "low":
                 {
                     AudioListener.volume = 0;
-                }break;
+                    PlayerPrefs.SetFloat("volume", 0);
+                }
+                break;
             case "mid":
                 {
                     AudioListener.volume = 0.5f;
+                    PlayerPrefs.SetFloat("volume", 0.5f);
+
                 }
                 break;
             case "high":
                 {
                     AudioListener.volume = 1;
+                    PlayerPrefs.SetFloat("volume", 1);
+                }
+                break;
+        }
+    }
+
+    public void ToggleSmoothMove(string input)
+    {
+        switch(input)
+        {
+            case "true":
+                {
+                    PlayerPrefs.SetInt("smoothmove", 1);
+                }break;
+            case "false":
+                {
+                    PlayerPrefs.SetInt("smoothmove", 0);
                 }
                 break;
         }
