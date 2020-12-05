f79c12f0d15c284221bbdbdb6b7277e7cb71a147
theishiopian <theishiopian@gmail.com>
2020-12-05 00:30:25 -0800

    Volume selector functional

--- Assets/OptionsMenu.cs
+++ Assets/OptionsMenu.cs
@@ -1,20 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class OptionsMenu : MonoBehaviour
 {
-    private void OnEnable()
-    {
-        if (!PlayerPrefs.HasKey("volume")) PlayerPrefs.SetFloat("volume", 1.0f);
-        if (!PlayerPrefs.HasKey("smoothmove")) PlayerPrefs.SetInt("smoothmove", 1);
+    public Transform volumeIndicator;
 
+    public Transform[] volumeNotches;
+
+    public TextMeshPro volumeText;
+
+    public bool debugMode = false;
+
+    void OnEnable()
+    {
+        if(debugMode)PlayerPrefs.DeleteAll();
+        if (!PlayerPrefs.HasKey("volume")) PlayerPrefs.SetFloat("volume", 1);
+        if (!PlayerPrefs.HasKey("smoothmove")) PlayerPrefs.SetInt("smoothmove", 1);
         AudioListener.volume = PlayerPrefs.GetFloat("volume");
+        //Debug.Log(PlayerPrefs.GetFloat("volume"));
+        MoveVolumeIndicator();
+        
+    }
+    
+    private int GetIndex()
+    {
+        int r = Mathf.FloorToInt((volumeNotches.Length - 1) * PlayerPrefs.GetFloat("volume"));
+        //Debug.Log(r);
+        return r;
+    }
+
+    private void MoveVolumeIndicator()
+    {
+        volumeIndicator.localPosition = new Vector3(volumeNotches[GetIndex()].localPosition.x, volumeIndicator.localPosition.y, volumeIndicator.localPosition.z);
+
+        volumeText.text = (PlayerPrefs.GetFloat("volume") * 100).ToString() + "%";
     }
 
     public void EditVolume(string input)
     {
-        switch(input)
+        switch (input)
         {
             case "low":
                 {
@@ -36,6 +63,9 @@ public class OptionsMenu : MonoBehaviour
                 }
                 break;
         }
+
+
+        MoveVolumeIndicator();
     }
 
     public void ToggleSmoothMove(string input)
