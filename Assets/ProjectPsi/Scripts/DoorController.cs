acd4beee9fc9774630345ff8c3cf66aadd4e46a5
theishiopian <theishiopian@gmail.com>
2020-12-05 21:42:45 -0800

    Fixed the door bug

--- Assets/ProjectPsi/Scripts/DoorController.cs
+++ Assets/ProjectPsi/Scripts/DoorController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 internal enum DoorState
 {
-    OPEN, CLOSED, LOCKED
+    OPEN, CLOSED, LOCKED, OPENING, CLOSING
 }
 
 public class DoorController : MonoBehaviour, ITriggerListener
@@ -14,86 +14,121 @@ public class DoorController : MonoBehaviour, ITriggerListener
     public AnimationCurve openCurve;
     public AnimationCurve closeCurve;
 
-    private bool moving = false;
+    //private bool moving = false;
+    private bool detected = false;
     private float t = 0;//0-1 closed-open
     private DoorState state = DoorState.CLOSED;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void Update()
     {
-        if(moving)
-        {
-            switch (state)
-            {
-                case DoorState.CLOSED://open door
-                    {
-                        MoveDoorPos(t, openCurve);
-                    }
-                    break;
-                case DoorState.OPEN://close door
-                    {
-                        MoveDoorPos(t, closeCurve);
-                    }
-                    break;
-            }
-            t += Time.deltaTime;
-            if(t >=1)
-            {
-                moving = false;
-                t = 0;
-                ToggleState();
-            }
-        }
+        //if(moving)
+        //{
+        //    switch (state)
+        //    {
+        //        case DoorState.CLOSING://open door
+        //            {
+        //                MoveDoorPos(t, closeCurve);
+        //            }
+        //            break;
+        //        case DoorState.OPENING://close door
+        //            {
+        //                MoveDoorPos(t, openCurve);
+        //            }
+        //            break;
+        //    }
+        //    t += Time.deltaTime;
+        //}
 
+        //Debug.Log(t);
+        //if (moving && t >= 1)
+        //{
+        //    moving = false;
+        //    t = 0;
+
+        //    switch (state)
+        //    {
+        //        case DoorState.OPENING: state = DoorState.OPEN; break;
+        //        case DoorState.CLOSING: state = DoorState.CLOSED; break;
+        //    }
+        //}
+
+        switch (state)
+        {
+            case DoorState.CLOSED:
+                {
+                    if(detected)
+                    {
+                        state = DoorState.OPENING;
+                    }
+                }
+                break;
+            case DoorState.OPENING:
+                {
+                    MoveDoorPos(openCurve);
+                    if(t>=1)
+                    {
+                        state = DoorState.OPEN;
+                        t = 0;
+                    }
+                }
+                break;
+            case DoorState.OPEN:
+                {
+                    if(!detected)
+                    {
+                        state = DoorState.CLOSING;
+                    }
+                }
+                break;
+            case DoorState.CLOSING:
+                {
+                    MoveDoorPos(closeCurve);
+                    if (t >= 1)
+                    {
+                        state = DoorState.CLOSED;
+                        t = 0;
+                    }
+                }
+                break;
+            case DoorState.LOCKED:
+                {
+                    //key check here
+                }
+                break;
+        }
     }
 
-    void MoveDoorPos(float t, AnimationCurve curve)
+    void MoveDoorPos(AnimationCurve curve)
     {
         door.localPosition = Vector3.Lerp(Vector3.zero, openPos, curve.Evaluate(t));
-    }
-
-    void ToggleState()
-    {
-        //Debug.Log("toggling");
-        if(state == DoorState.CLOSED)
-        {
-            state = DoorState.OPEN;
-            return;
-        }
-        if (state == DoorState.OPEN)
-        {
-            state = DoorState.CLOSED;
-            return;
-        }
+        t += Time.deltaTime;
     }
 
     public void OnEnter(Collider other)
     {
-        if(state != DoorState.LOCKED && !moving)
-        {
-            moving = true;
-            //ToggleState();
-        }
+        //if(!moving)
+        //{
+        //    state = DoorState.OPENING;
+        //    moving = true;
+        //}
+        detected = true;
     }
 
     public void OnExit(Collider other)
     {
-        if (state != DoorState.LOCKED && !moving)
-        {
-            moving = true;
-            //ToggleState();
-        }
+        //if(!moving)
+        //{
+        //    state = DoorState.CLOSING;
+        //    moving = true;
+        //}
+        detected = false;
     }
 
     public void OnStay(Collider other)
     {
         //throw new System.NotImplementedException();
+        detected = true;
     }
 
 }
