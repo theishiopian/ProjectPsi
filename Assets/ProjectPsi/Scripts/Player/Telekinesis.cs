e058632a2eeebbe4dd1b83547cfd5c1a8ed55fa6
theishiopian <theishiopian@gmail.com>
2021-02-13 18:35:25 -0800

    Implemented force grab
    BUGS:
    -targeting is very imprecise (todo: superior targeting system)
    -items seem to sometimes get frozen midair (pocket confict?)
    -large objects often take priority (see top)

--- Assets/ProjectPsi/Scripts/Player/Telekinesis.cs
+++ Assets/ProjectPsi/Scripts/Player/Telekinesis.cs
@@ -3,7 +3,6 @@ using Unity.Labs.SuperScience;
 using UnityEngine;
 using Valve.VR;
 using Valve.VR.InteractionSystem;
-using cakeslice;
 
 public class Telekinesis : MonoBehaviour
 {
@@ -11,9 +10,11 @@ public class Telekinesis : MonoBehaviour
     public SteamVR_Input_Sources controller;
     public SteamVR_Action_Boolean pickupAction;
     public SteamVR_Action_Boolean throwAction;
+    public Hand handScript;
 
     [Header("Transforms")]
     public Transform head;
+    public Transform hand;
 
     [Header("Physics Objects")]
     public SpringJoint joint;
@@ -24,40 +25,63 @@ public class Telekinesis : MonoBehaviour
     public float springMultiplier = 5;
     public float raycastRadius = 0.25f;
     public float raycastRange = 100f;
-    public string hitTag = "Grabbable";
+    public string liftTag = "Liftable";
+    public string grabTag = "Item";
+    public float grabForce = 25f;
+    public float grabDistance = 0.15f;
 
     RaycastHit hit;
-    private Rigidbody target;
+    private Rigidbody liftTarget;
+    private Rigidbody grabTarget;
 
     private void Update()
     {
+        Debug.LogFormat("LiftTarget is: {0}, GrabTarget is: {1}", liftTarget, grabTarget);
         if (Physics.SphereCast(head.transform.position, raycastRadius, head.forward, out hit, raycastRange, mask))
         {
-            if (hit.collider.CompareTag(hitTag))
+            if(hit.collider.CompareTag(grabTag))
             {
-                target = hit.rigidbody;
+                grabTarget = hit.rigidbody;
+                liftTarget = null;
+            }
+            else if (hit.collider.CompareTag(liftTag))
+            {
+                liftTarget = hit.rigidbody;
+                grabTarget = null;
             }
         }
 
-        if (pickupAction.GetStateDown(controller))
+        if(pickupAction.GetState(controller) && grabTarget)
         {
-            if(target)
+            grabTarget.AddForce((hand.position - grabTarget.transform.position).normalized * grabForce, ForceMode.Acceleration);
+
+            if(Vector3.Distance(hand.position, grabTarget.transform.position) < grabDistance)
             {
-                joint.connectedBody = target;
-                joint.spring = target.mass * springMultiplier;
+                //attatch to hand
+                handScript.AttachObject(grabTarget.gameObject, GrabTypes.Grip);
+
+                grabTarget = null;
             }
+        }
+
+        if (pickupAction.GetStateDown(controller) && liftTarget)
+        {
+            joint.connectedBody = liftTarget;
+            joint.spring = liftTarget.mass * springMultiplier;
         }
 
         if(pickupAction.GetStateUp(controller))
         {
             joint.connectedBody = null;
+            liftTarget = null;
+            grabTarget = null;
         }
 
         if(throwAction.GetStateDown(controller))
         {
             joint.connectedBody = null;
-            target.AddForce(head.forward * launchForce, ForceMode.VelocityChange);
-            target = null;
+            liftTarget.AddForce(head.forward * launchForce, ForceMode.VelocityChange);
+            liftTarget = null;
         }
     }
 }
