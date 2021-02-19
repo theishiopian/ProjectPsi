b2737de142c89ddd9527e8c442958f7cb08bc454
theishiopian <theishiopian@gmail.com>
2021-02-18 16:09:41 -0800

    Basic Stuff

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime.Tactical;
+
+public class PlayerHealth : MonoBehaviour, IDamageable
+{
+    public void Damage(float amount)
+    {
+        //throw new System.NotImplementedException();
+        return;
+    }
+
+    public bool IsAlive()
+    {
+        //throw new System.NotImplementedException();
+        return true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
