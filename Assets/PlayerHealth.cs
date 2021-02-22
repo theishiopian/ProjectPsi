3d4730cdbbe48b459138a8322b4410c6bf3dd48d
theishiopian <theishiopian@gmail.com>
2021-02-21 18:31:36 -0800

    more settup

---
+++ Assets/PlayerHealth.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using BehaviorDesigner.Runtime.Tactical;
+
+public class PlayerHealth : MonoBehaviour, IDamageable
+{
+    [Header("Tags (WARNING do not change at runtime)")]
+    public const string stunTag = "stun";
+    public const string killTag = "bullet";
+    public float startingHealth = 100;
+
+    public float Health { get; private set; }
+
+    public void Damage(float amount)
+    {
+        Health -= amount;
+    }
+
+    public bool IsAlive()
+    {
+        return true;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Health = startingHealth;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.collider.tag)
+        {
+            case stunTag:
+                {
+                    //stun
+                    break;
+                }
+            case killTag:
+                {
+                    Damage(10);
+                    break;
+                }
+        }
+
+    }
+}
