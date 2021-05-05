d5a00bdb3a832b91379d7c64853c676d80713c8c
theishiopian <theishiopian@gmail.com>
2021-05-05 16:12:31 -0700

    Added door break code

---
+++ Assets/ProjectPsi/Scripts/Level/DoorBreak.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorBreak : AbstractHealth
+{
+    public float armor = 30;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.impulse.magnitude > armor)
+        {
+            Damage(collision.impulse.magnitude * 0.01f);
+        }
+    }
+}
