4bb869f0eaabdfe7fa1e9d9b20ef76ccad156655
theishiopian <theishiopian@gmail.com>
2021-05-14 07:20:47 -0700

    gun can now shoot

--- Assets/ProjectPsi/Scripts/Turret.cs
+++ Assets/ProjectPsi/Scripts/Turret.cs
@@ -20,7 +20,9 @@ public class Turret : MonoBehaviour
 
     private GameObjectPool bullets;
     private bool tracking = false;
+    private bool firing = false;
     private Transform target;
+    private float fireTime = 0;
 
     private void Start()
     {
@@ -36,12 +38,43 @@ public class Turret : MonoBehaviour
             bracket.rotation = Quaternion.Lerp(bracket.rotation, Quaternion.LookRotation((target.position - transform.position).normalized, transform.up), Time.deltaTime * 3);
             bracket.localEulerAngles = new Vector3(0, bracket.localEulerAngles.y, 0);
             gun.localEulerAngles = new Vector3(gun.localEulerAngles.x, -90, 0);
+
+            RaycastHit hit;
+
+            bool didHit = Physics.Raycast(shootPoint.position, shootPoint.forward, out hit, losMask);
+
+            if(didHit && hit.collider.CompareTag("Player"))
+            {
+                StartFiring();
+            }
+            else
+            {
+                StopFiring();
+            }
         }
         else
         {
-
+            //gun.rotation = Quaternion.Lerp(gun.rotation, Quaternion.identity, Time.deltaTime * 3);
+            //bracket.rotation = Quaternion.Lerp(bracket.rotation, Quaternion.Euler(0,-90,0), Time.deltaTime * 3);
+            StopFiring();
         }
-        Debug.Log(tracking);
+        
+        if(firing)
+        {
+            fireTime += Time.deltaTime;
+
+            if(fireTime > 0.1f)
+            {
+                fireTime = 0;
+                Rigidbody bullet = bullets.Activate(shootPoint.position, shootPoint.rotation).GetComponent<Rigidbody>();
+
+                bullet.AddRelativeForce(0,0,10, ForceMode.Impulse);
+            }
+        }
+        else
+        {
+            fireTime = 0;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
@@ -60,5 +93,19 @@ public class Turret : MonoBehaviour
             tracking = false;
             target = null;
         }
+    }
+
+    private void StartFiring()
+    {
+        muzzleflash.Play();
+        shells.Play();
+        firing = true;
+    }
+
+    private void StopFiring()
+    {
+        muzzleflash.Stop();
+        shells.Stop();
+        firing = false;
     }
 }
