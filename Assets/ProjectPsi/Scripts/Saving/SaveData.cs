b821e4c023886a870b6066f2bdbca7a8e1565d76
theishiopian <theishiopian@gmail.com>
2021-05-15 21:29:21 -0700

    Cleanup and savedata object

---
+++ Assets/ProjectPsi/Scripts/Saving/SaveData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SaveData
+{
+    public Vector3 playerPosition;
+
+    public List<Vector3> enemyPositions;
+
+    public List<bool> doorsUnlocked;
+
+    public List<Vector3> cardPositions;
+}
